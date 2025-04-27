e9c558b98cb7b0be82e680b49a58d2b3d7e7b1d5
aisyah-trrd <aisyahtrrd@student.ub.ac.id>
2025-04-27 08:42:17 +0700

    tambah view

---
+++ view/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Awal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <div class="d-flex justify-content-center align-items-center min-vh-100">
+    <div class="container text-center">
+        <div class="border rounded d-flex justify-content-center align-items-center border-custom">
+            <img src="item.png">
+            Logo
+        </div>
+        <div>Tagline</div>
+        <p>................</p>
+        <button type="button" class="btn btn-secondary ">Pelanggan</button>
+        <button type="button" class="btn btn-secondary" onclick="location.href='?c=AppController&m=kategori'">Admin</button>
+    </div>
+    </div>
+</body>
+</html>
